94737c28e559b37965e67d8e06c155255d6c8696
carrot2803 <lezamadmitri@gmail.com>
2024-11-20 21:10:53 -0400

    Updated Base Models

--- App/models/course.py
+++ App/models/course.py
@@ -1,38 +1,25 @@
 from App.database import db
 
+
 class Course(db.Model):
-  __tablename__ = 'course'
+    course_code: str = db.Column(db.String(9), primary_key=True)
+    semester_id: int = db.Column(db.Integer, nullable=False)
+    course_title: str = db.Column(db.String(120), nullable=False)
+    level: int = db.Column(db.Integer, nullable=False)
+    assessments = db.relationship("CourseAssessment", backref="course", lazy="joined")
 
-  courseCode = db.Column(db.String(9), primary_key=True)
-  courseTitle = db.Column(db.String(120), nullable=False)
-  description = db.Column(db.String(1024), nullable=False)
-  level = db.Column(db.Integer, nullable=False)
-  semester = db.Column(db.Integer, nullable=False)
-  aNum = db.Column(db.Integer, nullable=False, default=0)
-  # creates reverse relationship from Course back to Assessment to access assessments for a specific course
-  # assessmentsAssigned = db.relationship('assessment', backref=db.backref('assessment', lazy='joined'))
+    def __init__(
+        self, course_code: str, semester_id: int, course_title: str, level: int
+    ) -> None:
+        self.course_code = course_code
+        self.semester_id = semester_id
+        self.course_title = course_title
+        self.level = level
 
-  def __init__(self, courseCode, courseTitle, description, level, semester, aNum):
-    self.courseCode = courseCode
-    self.courseTitle = courseTitle
-    self.description = description
-    self.level = level
-    self.semester = semester
-    self.aNum = aNum
-
-  def to_json(self):
-    return {
-      "courseCode" : self.courseCode,
-      "courseTitle" : self.courseTitle,
-      "description" : self.description,
-      "level" : self.level,
-      "semester" : self.semester,
-      "aNum" : self.aNum,
-    }
-
-  #Add new Course
-  def addCourse(courseCode, courseTitle, description, level, semester, aNum):
-    newCourse = Course(courseCode, courseTitle, description, level, semester, aNum)
-    db.session.add(newCourse)  #add to db
-    db.session.commit()
-    return newCourse
+    def to_json(self) -> dict[str, str | int]:
+        return {
+            "course_code": self.course_code,
+            "semester_id": self.semester_id,
+            "course_title": self.course_title,
+            "level": self.level,
+        }
--- App/models/semester.py
+++ App/models/semester.py
@@ -1,24 +1,32 @@
+from datetime import date
 from App.database import db
 
+
 class Semester(db.Model):
-    __tablename__='semester'
-    id = db.Column(db.Integer, primary_key= True, autoincrement=True)
-    startDate = db.Column(db.Date,nullable=False)
-    endDate = db.Column(db.Date,nullable=False)
-    semNum = db.Column(db.Integer,nullable=False)
-    maxAssessments = db.Column(db.Integer,nullable=False)
+    id: int = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    start_date: date = db.Column(db.Date, nullable=False)
+    end_date: date = db.Column(db.Date, nullable=False)
+    semester_number: int = db.Column(db.Integer, nullable=False)
+    max_assessments: int = db.Column(db.Integer, nullable=False)
+    coursesOffered = db.relationship("Course", backref="semester", lazy="joined")
 
-def __init__(self, startDate, endDate, semNum, maxAssessments):
-    self.startDate = startDate
-    self.endDate = endDate
-    self.semNum = semNum
-    self.maxAssessments = maxAssessments
+    def __init__(
+        self,
+        start_date: date,
+        end_date: date,
+        semester_number: int,
+        max_assessments: int,
+    ) -> None:
+        self.start_date = start_date
+        self.end_date = end_date
+        self.semester_number = semester_number
+        self.max_assessments = max_assessments
 
-def to_json(self):
-    return{
-        "id":self.id,
-        "startDate":self.startDate,
-        "endDate":self.endDate,
-        "semNum":self.semNum,
-        "maxAssessments":self.maxAssessments
-    }
+    def to_json(self) -> dict[str, int | str | date]:
+        return {
+            "id": self.id,
+            "startDate": self.start_date,
+            "endDate": self.end_date,
+            "semesterNumber": self.semester_number,
+            "maxAssessments": self.max_assessments,
+        }
