9c8eb78861f4085f27881d483c8fc7db3aff6d3a
Dmitri <96326986+carrot2803@users.noreply.github.com>
2024-11-20 21:20:27 -0400

    Updated Base Models (#4)

--- App/models/programme.py
+++ App/models/programme.py
@@ -1,18 +1,13 @@
 from App.database import db
 
+
 class Programme(db.Model):
-  __tablename__ = 'programme'
+    id: int = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name: str = db.Column(db.String(120), nullable=False)
+    courses = db.relationship("CourseProgramme")
 
-  p_ID = db.Column(db.Integer, primary_key = True, nullable=False, autoincrement=True)
-  p_name = db.Column(db.String(100), nullable = False)
-  #creates reverse relationship from Programme back to Course to access courses offered in a programme
-  programmeCourses = db.relationship('CourseProgramme', backref='courses', lazy='joined' )
+    def __init__(self, name) -> None:
+        self.name = name
 
-  def __init__(self, p_name):
-    self.p_name = p_name
-
-  def to_json(self):
-    return {
-    "p_ID" : self.p_ID,
-    "name" : self.p_name
-    } 
+    def to_json(self) -> dict[str, str | int]:
+        return {"id": self.id, "name": self.name}
