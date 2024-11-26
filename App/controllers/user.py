8a010f50c88049a46f15bb8c18d1b9a48e3b9759
meiying13 <FeliciaMeiying13@gmail.com>
2024-11-26 04:19:54 -0400

    updated user controller

--- App/controllers/user.py
+++ App/controllers/user.py
@@ -1,12 +1,17 @@
-from App.models import User, Admin
+from App.models import User, Admin, Staff
 
 
 def get_user(id: str) -> User | None:
-    user: User | None = User.query.get(id)
+    user: User | None = Admin.query.get(id) or Staff.query.get(id)
     if user is None:
         return None
     return user
 
+def get_user_by_email(email: str) -> User | None:
+    user: User | None = Admin.query.filter_by(email=email).first() or Staff.query.filter_by(email=email).first()
+    if user is None:
+        return None
+    return user
 
 def is_admin(id: int) -> bool:
     return Admin.query.get(id)
--- App/views/auth.py
+++ App/views/auth.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, flash, redirect, request, jsonify, render_template, url_for, make_response
 from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, current_user, unset_jwt_cookies, set_access_cookies
 from flask_login import logout_user
+from werkzeug import Response
 from App.controllers.auth import login
+from App.controllers.user import is_admin
 from App.models.user import User
 from App.models.staff import Staff
 from App.models.admin import Admin
@@ -15,36 +17,34 @@ def get_login_page():
     
 @auth_views.route('/login', methods=['POST'])
 def login_action():
-    email = request.form.get('email')           #To be linked to login button
-    password = request.form.get('password')     #To be linked to login button
-    response = login_user(email, password)
-    if not response:
-        flash('Bad email or password given'), 401 
-    return response       
-
-def login_user(email, password):
-    admin_user=db.session.query(Admin).filter(Admin.email == email).first()
-    if admin_user and admin_user.check_password(password):
-        response = make_response(redirect(url_for('admin_views.get_upload_page')))    
-        token = create_access_token(identity=email)
-        response.set_cookie('access_token', token)
-        return response
+    email: str | None = request.form.get('email')           #To be linked to login button
+    password: str | None = request.form.get('password')     #To be linked to login button
+    token: str | None = login_user(email, password)
+    if token is None:
+        flash('Bad email or password given'), 401
+        return get_login_page()
+    admin: Admin | None = Admin.query.filter_by(email=email).first()
+    if admin:
+        response: Response = redirect(url_for('admin_views.get_upload_page'))
     else:
-        user = db.session.query(Staff).filter(Staff.email == email).first()
-        if user and user.check_password(password):
-            response = make_response(redirect(url_for('staff_views.get_calendar_page')))
-            token = create_access_token(identity=email)
-            response.set_cookie('access_token', token)
-            return response
-    return jsonify(message="Invalid username or password"), 401
+        response: Response = redirect(url_for('staff_views.get_calendar_page'))  
+    set_access_cookies(response, token) 
+    return response           
+
+def login_user(email: str, password: str) -> str | None:
+    user: User | None = Admin.query.filter_by(email=email).first() or Staff.query.filter_by(email=email).first()
+    print("USER: ", user)
+    if user and user.check_password(password):
+        token: str = create_access_token(identity=email)
+        return token
+    return None
 
 @auth_views.route('/logout', methods=['GET'])
 @jwt_required()
 def logout():
-    email=get_jwt_identity()
-    print(email)
-    logout_user()
-    return render_template('login.html')
+    response: Response = redirect(url_for("auth_views.get_login_page"))
+    unset_jwt_cookies(response)
+    return response
 
 # @auth_views.route('/identify')
 # @jwt_required()
