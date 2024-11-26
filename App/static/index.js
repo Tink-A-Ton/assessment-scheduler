03f242a67f5069b53065db62c56a5db422d435f7
meiying13 <FeliciaMeiying13@gmail.com>
2024-11-26 17:31:23 -0400

    fixed calendar view

--- App/static/index.js
+++ App/static/index.js
@@ -1,297 +1,217 @@
-var weekCounter=0;
+var weekCounter = 0;
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
   const colors = {
-    Assignment:"#3397b9", 
-    Quiz:"#499373", 
-    Project:"#006064", 
-    Exam:"#CC4E4E", 
-    Presentation:"#cc7a50",
-    Other:"#C29203",
-    Pending: "#999999"
-  }
+    Assignment: "#3397b9",
+    Quiz: "#499373",
+    Project: "#006064",
+    Exam: "#CC4E4E",
+    Presentation: "#cc7a50",
+    Other: "#C29203",
+    Pending: "#999999",
+  };
 
-  const calendarEvents=[];
+  const calendarEvents = [];
+  renderCourses(myCourses, otherAssessments);
+  const levelFilter = document.getElementById("level");
+  const courseFilter = document.getElementById("courses");
   
-  const levelFilter = document.getElementById('level');
-  levelFilter.addEventListener('change', function(){
-    levelEvents=[];
-    const selectedValue = levelFilter.value;
-    otherAssessments.forEach((item) => {
-      code=item.course_code.replace(' ','');
-      if (code[4]==selectedValue){
-        if (item.start_date && item.end_date){ //if start and end date exists, apply event to calendar
-          const eventEl = document.createElement('div');
-          eventEl.classList.add('fc-event', 'fc-h-event', 'fc-daygrid-event', 'fc-daygrid-block-event');
-  
-          eventEl.dataset.color = '#800080';
-          eventEl.style.backgroundColor = '#800080';
-          
-          const eventObj={
-            id: item.id,
-            title: item.course_code+'-'+item.assessment_type,
-            backgroundColor: '#800080',
-            editable: false
-          };
-  
-          const isFullDay = item.start_time === '00:00:00' && (item.end_time === '23:59:00' || item.end_time === '00:00:00');
-          eventObj.start= item.start_date+'T'+item.start_time;
-          eventObj.end= item.end_date+'T'+item.end_time;
-          eventObj.allDay= isFullDay;
-          levelEvents.push(eventObj);
-        }
-      }
-    });
-    const allEvents = calendarEvents.concat(levelEvents);
-    calendar.setOption('events',allEvents);
-    calendar.render();
-  });
-
-  
-  const courseFilter = document.getElementById('courses');
-  courseFilter.addEventListener('change', function(){
-    courseEvents=[];
-    const selectedValue = courseFilter.value;
-    otherAssessments.forEach((item) => {
-      if (item.course_code==selectedValue){
-        if (item.start_date && item.end_date){ //if start and end date exists, apply event to calendar
-          const eventEl = document.createElement('div');
-          eventEl.classList.add('fc-event', 'fc-h-event', 'fc-daygrid-event', 'fc-daygrid-block-event');
-  
-          eventEl.dataset.color = '#800080';
-          eventEl.style.backgroundColor = '#800080';
-          
-          const eventObj={
-            id: item.id,
-            title: item.course_code+'-'+item.assessment_type,
-            backgroundColor: '#800080',
-            editable: false
-          };
-  
-          const isFullDay = item.start_time === '00:00:00' && (item.end_time === '23:59:00' || item.end_time === '00:00:00');
-          eventObj.start= item.start_date+'T'+item.start_time;
-          eventObj.end= item.end_date+'T'+item.end_time;
-          eventObj.allDay= isFullDay;
-          courseEvents.push(eventObj);
-        }
-      }
-    });
-    const allEvents = calendarEvents.concat(courseEvents);
-    calendar.setOption('events',allEvents);
-    calendar.render();
-  });
-
-
-
-  myCourses.forEach((course) => {
-    const courseCard = document.createElement('div');
-    courseCard.classList.add('course-card'); // Add styling for the course card     
-
-    const title = document.createElement('h3');
-    title.textContent = course.course_code;
-    courseCard.appendChild(title);
-
-    // Create a container for events within this course
-    const eventsContainer = document.createElement('div');
-    eventsContainer.classList.add('course-events'); // Add styling for the events container
-
-    // Loop through each assessment for the course and create an event element
-    assessments.forEach((a) => {
-      if (a.course_code==course.course_code){
-        const eventEl = document.createElement('div');
-        eventEl.classList.add('fc-event', 'fc-h-event', 'fc-daygrid-event', 'fc-daygrid-block-event');
-
-        const typeOfAssessment = getTypeOfAssessment(a.assessment_type);
-        var color = colors[typeOfAssessment];
-        if(a.clash_detected){
-          color=colors['Pending'];
-        }
-        eventEl.dataset.color = color;
-        eventEl.style.backgroundColor = color;
-        
-        const eventObj={
-          id: a.id,
-          title: course+'-'+a.assessment_type,
-          backgroundColor: color
-        };
-
-
-        if (a.start_date && a.end_date){ //if start and end date exists, apply event to calendar
-          const isFullDay = a.start_time === '00:00:00' && (a.end_time === '23:59:00' || a.end_time === '00:00:00');
-          eventObj.start= a.start_date+'T'+a.start_time;
-          eventObj.end= a.end_date+'T'+a.end_time;
-          eventObj.allDay= isFullDay;
-          calendarEvents.push(eventObj);
-        }
-        else{
-          eventEl.setAttribute('data-event-id',a.id);
-          eventEl.innerHTML = '<div class="fc-event-main">' + course + '-' + a.assessment_type + '</div>';
-          eventsContainer.appendChild(eventEl);
-        }
-
-      }  
-    });
-
-    courseCard.appendChild(eventsContainer);
-    document.getElementById('courses-list').appendChild(courseCard);
-  });
-
-    
-  var containerEl = document.getElementById('courses-list');      
-  new FullCalendar.Draggable(containerEl, {
-    itemSelector: '.fc-event',
-    eventData: function(eventEl) {
-      return {
-        title: eventEl.innerText.trim(),
-        backgroundColor: eventEl.dataset.color,
-        id: eventEl.dataset.eventId
-      }
-    }
-  });
-
-  var calendarEl = document.getElementById('calendar');
-  var calendar = new FullCalendar.Calendar(calendarEl, {
-    initialView: 'dayGridMonth',
+  const calendarEl = document.getElementById("calendar");
+  const calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: "dayGridMonth",
     headerToolbar: {
-      left:'prev,next,today',
-      center: 'title',    
-      right: 'semesterView,dayGridMonth,timeGridWeek,timeGridDay'
+      left: "prev,next,today",
+      center: "title",
+      right: "semesterView,dayGridMonth,timeGridWeek,timeGridDay",
+    },
+    views: {
+      semesterView: {
+        type: "dayGridMonth",
+        duration: { weeks: 13 },
+        buttonText: "Semester",
+        visibleRange: {
+          start: semester.start,
+          end: semester.end,
+        },
       },
-      views: {
-        semesterView: {
-          type: 'dayGridMonth',
-          duration: { weeks: 13 }, 
-          buttonText: 'Semester',
-          visibleRange: {
-            start: semester.start,
-            end: semester.end
-          }
-        }
-      },
-    editable:true,
-    selectable:true,
-    droppable:true,
+    },
+    editable: true,
+    selectable: true,
+    droppable: true,
     events: calendarEvents,
-    eventResize:function(info){
-      toEditItem(info.event);
-    },
-    eventDrop:function(info){
-      toEditItem(info.event);
-    },
-    drop: function(arg) {
-      newItem(arg);
-      arg.draggedEl.parentNode.removeChild(arg.draggedEl);
-    },
+    eventResize: handleEventEdit,
+    eventDrop: handleEventEdit,
+    drop: handleNewItem,
   });
   calendar.render();
-});
 
-
-function toEditItem(event){
-    let id=event.id;
-    let sDate=formatDate(new Date(event.start));
-    let eDate=formatDate(new Date(event.start));
-    if (event.end){
-      eDate=formatDate(new Date(event.end));
-    }
-    let sTime=formatTime(new Date(event.start));
-    let eTime=formatTime(new Date(event.end));
-
-    if (event.allDay || sTime=='' || eTime==''){
-      sTime='00:00:00';
-      eTime='23:59:00';
-    }
-    //make ajax request to backend
-    $.ajax({
-      url:'/calendar',
-      method:'POST',
-      data:{
-        id:id,
-        start_date:sDate,
-        end_date:eDate,
-        start_time:sTime,
-        end_time:eTime
-      },
-      success: function(response) {
-        location.reload(); // Reload the page
-      },
-      error: function(xhr, status, error) {
-          console.error('Error:', error);
-      }
-    });
-}
-
-function newItem(event){
-  let id=event.draggedEl.dataset.eventId;
-  let sDate=formatDate(new Date(event.date));
-  let eDate=formatDate(new Date(event.date));
-  let sTime=formatTime(new Date(event.date));
-  let eTime=formatTime(new Date(event.date));
-
-  if (event.allDay || sTime=='' || eTime==''){
-    sTime='00:00:00';
-    eTime='23:59:00';
-  }
-  console.log(id,sDate,eDate,sTime,eTime);
-  //make ajax request to backend
-  $.ajax({
-    url:'/calendar',
-    method:'POST',
-    data:{
-      id:id,
-      start_date:sDate,
-      end_date:eDate,
-      start_time:sTime,
-      end_time:eTime
-    },
-      success: function(response) {
-        location.reload(); // Reload the page
-      },
-      error: function(xhr, status, error) {
-          console.error('Error:', error);
-      }
+  levelFilter.addEventListener("change", function () {
+    const selectedLevel = levelFilter.value;
+    const filteredEvents = filterEventsByLevel(selectedLevel, otherAssessments);
+    updateCalendarEvents(calendar, filteredEvents);
   });
-}
 
+  courseFilter.addEventListener("change", function () {
+    const selectedCourse = courseFilter.value;
+    const filteredEvents = filterEventsByCourse(selectedCourse, otherAssessments);
+    updateCalendarEvents(calendar, filteredEvents);
+  });
 
-function formatDate(dateObj){
-  let year=dateObj.getFullYear();
-  let month=dateObj.getMonth()+1;
-  let day=dateObj.getDate();
+  function renderCourses(courses, assessments) {
+    const containerEl = document.getElementById("courses-list");
+    courses.forEach((course) => {
+      const courseCard = document.createElement("div");
+      courseCard.classList.add("course-card");
 
-  let paddedMonth=month.toString()
-  if (paddedMonth.length < 2){
-      paddedMonth="0"+paddedMonth;
+      const title = document.createElement("h3");
+      title.textContent = course.course_code;
+      courseCard.appendChild(title);
+
+      const eventsContainer = document.createElement("div");
+      eventsContainer.classList.add("course-events");
+
+      assessments
+        .filter((a) => a.course_code === course.course_code)
+        .forEach((a) => {
+          const eventEl = createEventElement(a, course.course_code, colors);
+          if (a.start_date && a.end_date) {
+            console.log(a);
+            const eventObj = createEventObject(a, colors);
+            calendarEvents.push(eventObj);
+          } else {
+            eventsContainer.appendChild(eventEl);
+          }
+        });
+      courseCard.appendChild(eventsContainer);
+      containerEl.appendChild(courseCard);
+    });
+
+    new FullCalendar.Draggable(containerEl, {
+      itemSelector: ".fc-event",
+      eventData: (eventEl) => ({
+        title: eventEl.innerText.trim(),
+        backgroundColor: eventEl.dataset.color,
+        id: eventEl.dataset.eventId,
+      }),
+    });
   }
 
-  let paddedDate=day.toString()
-  if (paddedDate.length < 2){
-      paddedDate="0"+paddedDate;
+  function createEventElement(assessment, course_code, colors) {
+    const typeOfAssessment = getAssessmentType(assessment.assessment_type.id);
+    const color = assessment.clash_detected ? colors.Pending : colors[typeOfAssessment];
+
+    const eventEl = document.createElement("div");
+    eventEl.classList.add(
+      "fc-event",
+      "fc-h-event",
+      "fc-daygrid-event",
+      "fc-daygrid-block-event"
+    );
+    eventEl.dataset.color = color;
+    eventEl.style.backgroundColor = color;
+    eventEl.setAttribute("data-event-id", assessment.id);
+    eventEl.innerHTML = '<div class="fc-event-main">' + course_code + '-' + assessment.assessment_type.category + '</div>';
+    return eventEl;
   }
 
-  let toStoreDate=`${year}-${paddedMonth}-${paddedDate}`;
-  return toStoreDate;
-}
-
-function formatTime(timeObj){
-  let hours = timeObj.getHours().toString().padStart(2, '0');
-  let minutes = timeObj.getMinutes().toString().padStart(2, '0');
-  let seconds = timeObj.getSeconds().toString().padStart(2, '0');
-  let toStoreTime=`${hours}:${minutes}:${seconds}`;
-  return toStoreTime;
-}
-
-function getTypeOfAssessment(eventName) {
-  if (eventName.toLowerCase().includes("assignment")) {
-      return "Assignment";
-  } else if (eventName.toLowerCase().includes("exam")) {
-      return "Exam";
-  } else if (eventName.toLowerCase().includes("project")) {
-      return "Project";
-  } else if (eventName.toLowerCase().includes("quiz")) {
-      return "Quiz";
-  } else if (eventName.toLowerCase().includes("presentation")) {
-      return "Presentation";
+  function createEventObject(assessment, colors) {
+    const typeOfAssessment = getAssessmentType(assessment.assessment_type.id);
+    const isFullDay =
+      assessment.start_time === "00:00" &&
+      (assessment.end_time === "23:59" || assessment.end_time === "00:00");
+    
+    return {
+      id: assessment.id,
+      title: `${assessment.course_code}-${typeOfAssessment}`,
+      backgroundColor: colors[typeOfAssessment],
+      start: `${assessment.start_date}T${assessment.start_time}`,
+      end: `${assessment.end_date}T${assessment.end_time}`,
+      allDay: isFullDay,
+    };
   }
-  return "Other";
-}
+
+  function filterEventsByLevel(level, assessments) {
+    return level === "0" ? assessments : assessments.filter((item) => item.course_code[4] === level);
+  }
+
+  function filterEventsByCourse(courseCode, assessments) {
+    return courseCode == "all" ? assessments : assessments.filter((item) => item.course_code === courseCode);
+  }
+
+  function updateCalendarEvents(calendar, newEvents) {
+    calendar.removeAllEvents();
+    const updatedEvents = newEvents.map((a) => createEventObject(a, colors));
+    calendar.setOption("events", updatedEvents);
+    calendar.render();
+    console.log(updatedEvents);
+  }
+
+  function handleEventEdit(info) {
+    const event = info.event;
+    const data = extractEventDetails(event);
+    saveEvent(data);
+  }
+
+  function handleNewItem(arg) {
+    const event = arg.draggedEl;
+    const data = {
+      id: event.dataset.eventId,
+      startDate: formatDate(arg.date),
+      endDate: formatDate(arg.date),
+      startTime: "00:00",
+      endTime: "23:59",
+    };
+    saveEvent(data);
+    arg.draggedEl.parentNode.removeChild(arg.draggedEl);
+  }
+
+  function extractEventDetails(event) {
+    const id = event.id;
+    const startDate = formatDate(new Date(event.start));
+    const endDate = event.end ? formatDate(new Date(event.end)) : startDate;
+    const startTime = formatTime(new Date(event.start));
+    const endTime = event.end ? formatTime(new Date(event.end)) : "23:59";
+
+    return { id, startDate, endDate, startTime, endTime };
+  }
+
+  function saveEvent(data) {
+    $.ajax({
+      url: "/calendar",
+      method: "POST",
+      data,
+      success: () => location.reload(),
+      error: (xhr, status, error) => console.error("Error:", error),
+    });
+  }
+
+  function getAssessmentType(typeId) {
+    switch (typeId) {
+      case 1:
+        return "Assignment";
+      case 2:
+        return "Exam";
+      case 3:
+        return "Quiz";
+      case 4:
+        return "Project";
+      case 5:
+        return "Presentation";
+      default:
+        return "Other";
+    }
+  }
+
+  function formatDate(dateObj) {
+    const year = dateObj.getFullYear();
+    const month = (dateObj.getMonth() + 1).toString().padStart(2, "0");
+    const day = dateObj.getDate().toString().padStart(2, "0");
+    return `${year}-${month}-${day}`;
+  }
+
+  function formatTime(dateObj) {
+    const hours = dateObj.getHours().toString().padStart(2, "0");
+    const minutes = dateObj.getMinutes().toString().padStart(2, "0");
+    return `${hours}:${minutes}`;
+  }
+});
