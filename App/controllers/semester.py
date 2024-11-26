bf8cde8fbb79a5a34bbf92a215a1a0ea3406ebce
meiying13 <FeliciaMeiying13@gmail.com>
2024-11-26 18:12:38 -0400

    renamed to add_semester

--- App/controllers/semester.py
+++ App/controllers/semester.py
@@ -2,7 +2,7 @@ from App.models import Semester
 from App.database import db
 
 
-def add_sem(start_date, end_date, semester_number, max_assessments) -> Semester:
+def add_semester(start_date, end_date, semester_number, max_assessments) -> Semester:
     new_sem = Semester(
         start_date=start_date,
         end_date=end_date,
