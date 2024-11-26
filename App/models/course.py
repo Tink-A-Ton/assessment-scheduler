ca31710fb6c9516c7f86e3ea1f6a5ebbf59f7ec5
arashi009 <brandonchandoo66@gmail.com>
2024-11-25 21:26:48 -0400

    refactored course controller to align with updated model

--- App/models/course.py
+++ App/models/course.py
@@ -6,7 +6,7 @@ class Course(db.Model):
     semester_id: int = db.Column(db.Integer, nullable=False)
     course_title: str = db.Column(db.String(120), nullable=False)
     level: int = db.Column(db.Integer, nullable=False)
-    assessments = db.relationship("CourseAssessment", backref="course", lazy="joined")
+    Assessments = db.relationship("Assessment", backref="course", lazy="joined")
 
     def __init__(
         self, course_code: str, semester_id: int, course_title: str, level: int
