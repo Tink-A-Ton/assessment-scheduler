40dcaef6af42e4735b1cf15dc39fd2f08ac9a00b
meiying13 <FeliciaMeiying13@gmail.com>
2024-11-28 22:17:17 -0400

    refactored controllers for domain model handling

--- App/controllers/course.py
+++ App/controllers/course.py
@@ -2,7 +2,7 @@ from App.models import Course
 from App.database import db
 
 def add_course(course_code: str, course_title: str, level: int, semester: int) -> bool:
-    existing_course = Course.query.filter_by(course_code=course_code).first()
+    existing_course: Course | None = Course.query.get(course_code)
     if existing_course is not None:
         return False
     new_course = Course(course_code=course_code, semester_id=semester, course_title=course_title, level=level)
@@ -14,10 +14,10 @@ def get_all_courses() -> list[Course]:
     return Course.query.all() 
 
 def get_course(course_code: str) -> Course | None:
-    return Course.query.filter_by(course_code=course_code).first()
+    return Course.query.get(course_code)
 
 def edit_course(course_code: str, semester_id: int, course_title: str, level: int) -> Course | None:
-    existing_course = Course.query.filter_by(course_code=course_code).first()
+    existing_course: Course | None = Course.query.get(course_code)
     if existing_course:
         existing_course.semester_id = semester_id
         existing_course.course_title = course_title
@@ -27,17 +27,17 @@ def edit_course(course_code: str, semester_id: int, course_title: str, level: in
     return None    
 
 def get_course_level(course_code: str) -> int | None:
-    course = Course.query.filter_by(course_code=course_code).first()
+    course: Course | None = Course.query.get(course_code)
     if course:
         return course.level
     return None
 
 def get_courses_by_level(level: int) -> list[Course] | None:
-    return Course.query.filter_by(level = level).all()
+    return Course.query.filter_by(level=level).all()
 
 
 def delete_course(course_code: str) -> bool:
-    course_to_delete = Course.query.get(course_code)
+    course_to_delete: Course | None = Course.query.get(course_code)
     if course_to_delete:
         db.session.delete(course_to_delete)
         db.session.commit()
