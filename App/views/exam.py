4c44a014cd96d65eeb994b25c97dde53ed95598d
carrot2803 <lezamadmitri@gmail.com>
2024-11-30 16:28:44 -0400

    update

--- App/views/exam.py
+++ App/views/exam.py
@@ -1,10 +1,6 @@
 from flask import Blueprint, request, render_template, redirect, url_for, flash
 from werkzeug import Response
-from App.database import db
-from flask_jwt_extended import get_jwt_identity
-from flask_jwt_extended import jwt_required
-from datetime import date, time
-import time as sleep_timer
+from flask_jwt_extended import get_jwt_identity, jwt_required
 from ..models import Course, Exam, Staff
 from ..controllers import (
     get_exam,
@@ -23,15 +19,10 @@ from App.models import Staff
 exam_views = Blueprint("exam_views", __name__, template_folder="../templates")
 
 
-def get_current_staff() -> Staff:
-    return get_staff(get_jwt_identity())
-
-
 @exam_views.route("/assessments", methods=["GET"])
 @jwt_required(Staff)
 def get_exams_page() -> str:
-    staff: Staff | None = get_current_staff()
-    staff_courses: list[dict] = get_staff_courses(staff.id)
+    staff_courses: list[dict] = get_staff_courses(get_jwt_identity())
     exams: list[dict] = [
         exam.to_json()
         for course in staff_courses
@@ -43,19 +34,14 @@ def get_exams_page() -> str:
 @exam_views.route("/modifyAssessment/<int:id>", methods=["GET"])
 @jwt_required(Staff)
 def get_modify_exam_page(id) -> str:
-    exam: Exam | None = get_exam(id)
-    return render_template("modifyAssessment.html", course_assessment=exam)
+    return render_template("modifyAssessment.html", course_assessment=get_exam(id))
 
 
 @exam_views.route("/addAssessment", methods=["GET"])
 @jwt_required(Staff)
 def get_add_exams_page() -> str:
-    staff: Staff | None = get_current_staff()
-    registered_courses: list[Course] = get_registered_courses(staff.id)
-    return render_template(
-        "addAssessment.html",
-        courses=registered_courses,
-    )
+    courses: list[Course] = get_registered_courses(get_jwt_identity())
+    return render_template("addAssessment.html", courses=courses)
 
 
 @exam_views.route("/addAssessment", methods=["POST"])
@@ -69,12 +55,9 @@ def add_exam_action() -> Response | str:
         parse_time(data["endTime"]),
         False,
     )
-    clash: bool = detect_exam_clash(
-        exam, request.form.get("rule1"), request.form.get("rule2")
-    )
+    clash: bool = detect_exam_clash(exam, data["rule1"], data["rule2"])
     if clash:
         flash("Clash Detected! Reschedule exam or send to ADMIN for approval")
-        sleep_timer.sleep(1)
     return redirect(url_for("exam_views.get_exams_page"))
 
 
@@ -82,16 +65,13 @@ def add_exam_action() -> Response | str:
 @jwt_required(Staff)
 def modify_exam_action(id) -> Response:
     data: dict[str, str] = request.form
-    exam: None | Exam = update_exam(
+    exam: Exam | None = update_exam(
         id, data["startDate"], data["startTime"], data["endTime"]
     )
-    if exam:
-        flash(f"Exam Details Updated !")
-        clash: bool = detect_exam_clash(
-            exam, request.form.get("rule1"), request.form.get("rule2")
-        )
-        if clash:
-            flash("Clash Detected! Reschedule exam or send to ADMIN for approval")
+    flash(f"Exam Details Updated !")
+    clash: bool = detect_exam_clash(exam, data["rule1"], data["rule2"])
+    if clash:
+        flash("Clash Detected! Reschedule exam or send to ADMIN for approval")
     return redirect(url_for("exam_views.get_exams_page"))
 
 
