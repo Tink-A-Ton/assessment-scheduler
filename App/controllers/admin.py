a41cb418e5cfbb4495574e21a92b0834e0b9b620
felicia <FeliciaMeiying13@gmail.com>
2024-12-02 16:02:08 -0400

    15.2 refactored admin pages (#43)
    refactored admin pages
    
    - updated semester page
    - updated courses pages
    - update clashes page
    - added mobile scaling
    ---------
    Co-authored-by: carrot2803 <lezamadmitri@gmail.com>

--- App/controllers/admin.py
+++ App/controllers/admin.py
@@ -2,7 +2,7 @@ import os, csv
 from datetime import date, datetime
 from werkzeug.utils import secure_filename
 from ..models import Admin
-from .exam import get_exam, get_exams
+from .exam import get_exam
 from ..models import Exam
 from .course import create_course
 from ..database import db
@@ -13,6 +13,7 @@ def allow_override(id: int) -> None:
     exam: Exam | None = get_exam(id)
     if exam:
         exam.clash_detected = False
+        db.session.commit()
 
 
 def deny_override(id: int) -> None:
@@ -40,13 +41,6 @@ def process_file(file) -> None:
                 int(row["Level"]),
                 int(semester["id"]),
             )
-
-
-def get_search_results(day: str | None) -> list[Exam]:
-    if not day:
-        return []
-    start_date: date = datetime.strptime(day, "%Y-%m-%d").date()
-    return [a for a in get_exams() if start_date <= a.start_date]
 
 
 def create_admin(id, email, password) -> Admin:
