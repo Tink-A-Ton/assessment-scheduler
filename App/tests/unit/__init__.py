536aa49dbdc76fa522ac32b6cbeaada0216050a0
arashi009 <brandonchandoo66@gmail.com>
2024-12-01 21:40:07 -0400

    organized unit tests into directory

---
+++ App/tests/unit/__init__.py
@@ -0,0 +1,8 @@
+from .clash_context_unit_test import *  
+from .exam_unit_test import *
+from .instructor_unit_test import *
+from .programme_unit_test import *
+from .programmeCourse_unit_test import *
+from .semester_unit_test import *
+from .user_unit_test import *
+
