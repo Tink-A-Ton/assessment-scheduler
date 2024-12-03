7e47c32e122c43f39cff55411fce0850db3e9a57
meiying13 <FeliciaMeiying13@gmail.com>
2024-12-03 03:47:38 -0400

    moved parse_date, parse_time to utils

--- App/models/utils.py
+++ App/models/utils.py
@@ -1,4 +1,4 @@
-from datetime import date, timedelta
+from datetime import date, datetime, time, timedelta
 from enum import Enum
 
 
@@ -18,3 +18,16 @@ def get_week_range(date_str: str) -> tuple[date, date]:
     start_of_week: date = input_date - timedelta(days=(input_date.weekday() + 1) % 7)
     end_of_week: date = start_of_week + timedelta(days=6)
     return start_of_week, end_of_week
+
+
+def parse_date(date_str) -> None | date:
+    if date_str == "":
+        return None
+    return datetime.strptime(date_str, "%Y-%m-%d").date()
+
+
+def parse_time(time_str) -> None | time:
+    if time_str == "":
+        return None
+    time_str = time_str[:5]
+    return datetime.strptime(time_str, "%H:%M").time()
