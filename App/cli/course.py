0f4fabe96737cf483aabb4cdc4a4e21d01cae274
meiying13 <FeliciaMeiying13@gmail.com>
2024-12-04 21:31:26 -0400

    cleaned up code

--- App/cli/course.py
+++ App/cli/course.py
@@ -2,8 +2,8 @@ import click
 from flask.cli import AppGroup
 from rich.console import Console
 from rich.table import Table
-from App.controllers import get_courses,create_course,get_course
-from flask import current_app
+from ..controllers import get_courses, create_course
+from ..models import Course
 from .utils import course_not_exist_checker
 
 console = Console()
@@ -13,28 +13,33 @@ course = AppGroup("course", help="Commands that relate to the management of cour
 @course.command("create", help="Creates a new course")
 def assign_course():
     console.print("\n")
-    course_code = course_not_exist_checker(None,None,click.prompt("Course Code", default="COMP1605", show_default=True))
-    course_title = click.prompt("course_title", default="Computer Fantasy", show_default=True)
+    course_code = course_not_exist_checker(
+        None, None, click.prompt("Course Code", default="COMP1605", show_default=True)
+    )
+    course_title = click.prompt(
+        "course_title", default="Computer Fantasy", show_default=True
+    )
     level = click.prompt("level", default=1, show_default=True)
     semester = click.prompt("semester", default=1, show_default=True)
-    level = int(level)
-    semester = int(semester)
-    if create_course(course_code, course_title, level, semester):
+    if create_course(course_code, course_title, int(level), int(semester)):
         console.print(f"[green]Course with code {course_code} added successfully")
     else:
         console.print(f"[red]Failed to add Course with code {course_code}")
     console.print("\n")
 
+
 @course.command("list", help="Lists all existing courses")
-def ls():
+def list_courses():
     console.print("\n")
-    courses = get_courses()
+    courses: list[Course] = get_courses()
     table = Table(title="Existing Courses")
     table.add_column("Course Code", style="yellow")
     table.add_column("Course Title", style="cyan")
     table.add_column("Semester ID", style="cyan")
     table.add_column("Level", style="cyan")
     for c in courses:
-        table.add_row(str(c.course_code), c.course_title, str(c.semester_id), str(c.level))
+        table.add_row(
+            str(c.course_code), c.course_title, str(c.semester_id), str(c.level)
+        )
     console.print(table)
     console.print("\n")
